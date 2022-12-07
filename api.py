6451342e3e78ddd0615b3948eb99a5e9367daf11
Ashwin <arihant2math@gmail.com>
2022-12-07 12:08:01 -0800

    pyinstaller does not like cython

--- api.py
+++ api.py
@@ -1,9 +1,6 @@
-import cython
-
 import api_keys
 
 
-@cython.compile
 def weather(location, metric):
     out = api_keys.OPEN_WEATHER_MAP_API_URL + "weather?lat=" + str(location[0]) + "&lon=" + str(
         location[1]) + "&appid=" + api_keys.OPEN_WEATHER_MAP_API_KEY
@@ -13,7 +10,6 @@ def weather(location, metric):
         return out + "&units=metric"
 
 
-@cython.compile
 def air_quality(location, metric):
     return (
             api_keys.OPEN_WEATHER_MAP_API_URL + "air_pollution?lat=" + str(location[0]) + "&lon=" + str(location[1]) +
@@ -21,7 +17,6 @@ def air_quality(location, metric):
             + "&units=imperial")
 
 
-@cython.compile
 def forecast(location, metric):
     out = api_keys.OPEN_WEATHER_MAP_API_URL + "forecast?lat=" + str(location[0]) + "&lon=" + str(
         location[1]) + "&appid=" + api_keys.OPEN_WEATHER_MAP_API_KEY
