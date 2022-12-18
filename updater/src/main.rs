259c712bc8e1fd1e753e14bde924a53298131e9b
Ashwin <arihant2math@gmail.com>
2022-12-18 12:18:24 -0800

    Updates

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     let data = reqwest::blocking::get("https://arihant2math.github.io/weathercli/weather.exe").expect("url expected").bytes().expect("bytes expected");
     let mut file = OpenOptions::new().write(true).open("weather.exe").unwrap();
     file.write_all(&data).expect("Something went wrong opening the file");
+    println!("Update finished");
 }
