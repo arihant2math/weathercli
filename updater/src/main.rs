d8b1f414ae7e90c1d859dc6b7aa1e9c51a7d5dff
Ashwin <arihant2math@gmail.com>
2022-12-18 12:41:04 -0800

    Changed icon + Added unix support to updater + fix gh issues

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -2,9 +2,20 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 fn main() {
-    println!("Downloading latest version ...");
-    let data = reqwest::blocking::get("https://arihant2math.github.io/weathercli/weather.exe").expect("url expected").bytes().expect("bytes expected");
-    let mut file = OpenOptions::new().write(true).open("weather.exe").unwrap();
-    file.write_all(&data).expect("Something went wrong opening the file");
-    println!("Update finished");
+    if cfg!(windows) {
+        println!("Downloading latest version ...");
+        let data = reqwest::blocking::get("https://arihant2math.github.io/weathercli/weather.exe").expect("url expected").bytes().expect("bytes expected");
+        let mut file = OpenOptions::new().write(true).open("weather.exe").unwrap();
+        file.write_all(&data).expect("Something went wrong opening the file");
+        println!("Update finished");
+    }
+    else if  cfg!(unix) {
+        let data = reqwest::blocking::get("https://arihant2math.github.io/weathercli/weather").expect("url expected").bytes().expect("bytes expected");
+        let mut file = OpenOptions::new().write(true).open("weather").unwrap();
+        file.write_all(&data).expect("Something went wrong opening the file");
+        println!("Update finished");
+    }
+    else {
+        println!("OS unsupported");
+    }
 }
