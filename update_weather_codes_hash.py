1a0e7a81d9a41bd52a7d77576e6e3704746f0b7e
Ashwin Naren <arihant2math@gmail.com>
2023-01-20 23:43:03 -0800

    added weather codes hash updater

---
+++ update_weather_codes_hash.py
@@ -0,0 +1,10 @@
+import json
+import os.path
+
+from core import hash_file
+
+f = json.load(open('./weather_codes.json'))
+file_hash = hash_file(os.path.abspath('./weather_codes.json'))
+index_json = json.load(open('./docs_templates/index.json'))
+index_json['weather-codes-hash'] = file_hash
+json.dump(index_json, open('./docs_templates/index.json', 'w'))
