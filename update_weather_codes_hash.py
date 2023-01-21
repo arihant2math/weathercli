6e4aeeb854ed12fcbf8406579a521fe251c68e10
Ashwin Naren <arihant2math@gmail.com>
2023-01-21 15:35:47 -0800

    formatting

--- update_weather_codes_hash.py
+++ update_weather_codes_hash.py
@@ -3,8 +3,8 @@ import os.path
 
 from core import hash_file
 
-f = json.load(open('./weather_codes.json'))
-file_hash = hash_file(os.path.abspath('./weather_codes.json'))
-index_json = json.load(open('./docs_templates/index.json'))
-index_json['weather-codes-hash'] = file_hash
-json.dump(index_json, open('./docs_templates/index.json', 'w'))
+f = json.load(open("./weather_codes.json"))
+file_hash = hash_file(os.path.abspath("./weather_codes.json"))
+index_json = json.load(open("./docs_templates/index.json"))
+index_json["weather-codes-hash"] = file_hash
+json.dump(index_json, open("./docs_templates/index.json", "w"))
