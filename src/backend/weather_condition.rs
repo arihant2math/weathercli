fe4f97f0d0b0e4e18c176ec6a02feabb38f61352
Ashwin Naren <arihant2math@gmail.com>
2023-03-07 20:49:19 -0800

    corelib: Added image url to weather_condition.rs
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- src/backend/weather_condition.rs
+++ src/backend/weather_condition.rs
@@ -6,28 +6,28 @@ use crate::weather_file::WeatherFile;
 #[pyclass(subclass)]
 #[derive(Clone)]
 pub struct WeatherCondition {
-    #[pyo3(get, set)]
-    pub(crate) condition_id: u16,
-    #[pyo3(get, set)]
+    #[pyo3(get)]
+    pub condition_id: u16,
+    #[pyo3(get)]
+    pub image_url: String,
+    #[pyo3(get)]
     pub sentence: String,
 }
 
-#[pyfunction]
-pub fn get_sentence(id: u16) -> String {
-    let f = WeatherFile::new("weather_codes.json".to_string());
-    let data: Value = serde_json::from_str(&f.data).expect("Json expected");
-    let code = &data[id.to_string()];
-    let sentence = code[3].clone();
-    sentence.as_str().expect("String expected").to_string()
-}
 
 #[pymethods]
 impl WeatherCondition {
     #[new]
     fn new(condition_id: u16) -> Self {
+        let f = WeatherFile::new("weather_codes.json".to_string());
+        let data: Value = serde_json::from_str(&f.data).expect("Json expected");
+        let code = &data[condition_id.to_string()];
+        let sentence = code[3].clone().as_str().expect("String expected").to_string();
+        let image_url = "https://openweathermap.org/img/wn/".to_string() + code[2].clone().as_str().expect("String expected") + "@4x.png";
         WeatherCondition {
             condition_id,
-            sentence: get_sentence(condition_id),
+            image_url,
+            sentence,
         }
     }
 }
