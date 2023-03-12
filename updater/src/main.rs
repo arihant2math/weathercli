807c1987f545a5b3d4dd5a7f55f7932196f98dfa
Ashwin Naren <arihant2math@gmail.com>
2023-03-11 16:28:44 -0800

    updater: fixed file in use bug with retries
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -1,6 +1,8 @@
 use std::cmp::min;
 use std::fs::File;
 use std::io::Write;
+use std::thread;
+use std::time::Duration;
 
 use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
@@ -20,7 +22,6 @@ async fn main() -> Result<(), String> {
         return Err("OS unsupported".to_string());
     }
     let client = Client::new();
-    println!("===== Updater =====");
     // Reqwest setup
     let res = client
         .get(url)
@@ -31,14 +32,26 @@ async fn main() -> Result<(), String> {
         .content_length()
         .ok_or(format!("Failed to get content length from '{}'", &url))?;
 
+    println!("\x1b[32m===== Updater =====\x1b[0m");
+
+    let mut file_expect = File::create(path);
+    let retries = 0;
+    while file_expect.is_err() {
+        if retries > 3 {
+            return Err(format!("Failed to create/open file '{}'", path));
+        }
+        file_expect = File::create(path);
+        thread::sleep(Duration::from_millis(1000));
+    }
+    let mut file = file_expect.unwrap();
+
     let progress_bar = ProgressBar::new(total_size);
     progress_bar.set_style(ProgressStyle::default_bar()
-        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})").expect("Failed due to Indicatif progress bar")
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})").expect("Failed due to progress bar error")
         .progress_chars("#>-"));
     progress_bar.set_message("Downloading ".to_string() + url);
 
     // download chunks
-    let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
     let mut downloaded: u64 = 0;
     let mut stream = res.bytes_stream();
 
