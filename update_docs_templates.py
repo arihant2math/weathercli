5261f1b1f2c1c6c320ad5e0c2e5bfcb9d68231aa
Ashwin Naren <arihant2math@gmail.com>
2023-03-12 11:55:14 -0700

    docs bump
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- update_docs_templates.py
+++ update_docs_templates.py
@@ -37,6 +37,7 @@ def filter_by_file(runs, file):
         run for run in runs if (run["path"] == file)
     ]
 
+
 @click.command()
 @click.argument("gh_token")
 def main(gh_token):
@@ -45,30 +46,40 @@ def main(gh_token):
     s = requests.Session()
     s.auth = ("token", gh_token)
     get_run_id = s.get(
-        "https://api.github.com/repos/arihant2math/weathercli/actions/runs"
+        "https://api.github.com/repos/arihant2math/weathercli/actions/runs?per_page=100&status=completed"
     )
     runs = get_run_id.json()["workflow_runs"]
     ci = filter_by_file(runs, ".github/workflows/build.yml")
     updater_ci = filter_by_file(runs, ".github/workflows/build-updater.yml")
     daemon_ci = filter_by_file(runs, ".github/workflows/build-daemon.yml")
-    latest_run_id = ci[0]["id"]
-    latest_updater_run_id = updater_ci[0]["id"]
-    latest_daemon_run_id = daemon_ci[0]["id"]
-    artifacts = get_artifact_urls(s, latest_run_id)
-    updater_artifacts = get_artifact_urls(s, latest_updater_run_id)
-    daemon_artifacts = get_artifact_urls(s, latest_daemon_run_id)
-    print("Starting Unix Download")
-    download_artifact(s, artifacts, "weather (Unix)", "weather")
-    print("Starting Windows Download")
-    download_artifact(s, artifacts, "weather (Windows)", "weather.exe")
-    print("Starting Unix Download (Updater)")
-    download_artifact(s, updater_artifacts, "updater (Unix)", "updater")
-    print("Starting Windows Download (Updater)")
-    download_artifact(s, updater_artifacts, "updater (Windows)", "updater.exe")
-    print("Starting Unix Download (Daemon)")
-    download_artifact(s, daemon_artifacts, "weatherd (Unix)", "weatherd")
-    print("Starting Windows Download (Daemon)")
-    download_artifact(s, daemon_artifacts, "weatherd (Windows)", "weatherd.exe")
+    artifacts = None
+    updater_artifacts = None
+    daemon_artifacts = None
+    if len(ci) > 0:
+        latest_run_id = ci[0]["id"]
+        artifacts = get_artifact_urls(s, latest_run_id)
+    if len(updater_ci) > 0:
+        latest_updater_run_id = updater_ci[0]["id"]
+        updater_artifacts = get_artifact_urls(s, latest_updater_run_id)
+    if len(daemon_ci) > 0:
+        latest_daemon_run_id = daemon_ci[0]["id"]
+        daemon_artifacts = get_artifact_urls(s, latest_daemon_run_id)
+
+    if artifacts is not None:
+        print("Starting Unix Download")
+        download_artifact(s, artifacts, "weather (Unix)", "weather")
+        print("Starting Windows Download")
+        download_artifact(s, artifacts, "weather (Windows)", "weather.exe")
+    if updater_artifacts is not None:
+        print("Starting Unix Download (Updater)")
+        download_artifact(s, updater_artifacts, "updater (Unix)", "updater")
+        print("Starting Windows Download (Updater)")
+        download_artifact(s, updater_artifacts, "updater (Windows)", "updater.exe")
+    if daemon_artifacts is not None:
+        print("Starting Unix Download (Daemon)")
+        download_artifact(s, daemon_artifacts, "weatherd (Unix)", "weatherd")
+        print("Starting Windows Download (Daemon)")
+        download_artifact(s, daemon_artifacts, "weatherd (Windows)", "weatherd.exe")
     shutil.rmtree("./tmp")
     d = json.load(open("./docs_templates/index.json"))
     now = datetime.datetime.now()
