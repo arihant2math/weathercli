1fe948b212dacfd631d8d48496659119f74f2668
Ashwin Naren <arihant2math@gmail.com>
2023-03-17 15:58:47 -0700

    removed geopy dependency
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- cli/location.py
+++ cli/location.py
@@ -1,12 +1,35 @@
-import ssl
+import json
 import threading
 
-import certifi
 import core
-import geopy
-from geopy import Bing, Nominatim, Location
 
 from cli.local import settings
+
+
+def bing_maps_location_query(query):
+    r = core.networking.get_url(
+        'http://dev.virtualearth.net/REST/v1/Locations?query="{}"&maxResults=5&key={}'
+        "".format(query, settings.BING_MAPS_API_KEY)
+    )
+    j = json.loads(r.text)["resourceSets"][0]["resources"][0]["point"]["coordinates"]
+    return j[0], j[1]
+
+
+def nominatim_geocode(query):
+    r = core.networking.get_url(
+        'https://nominatim.openstreetmap.org/search?q="{}"&format=jsonv2'.format(query)
+    )
+    j = json.loads(r.text)
+    return j[0]["lat"], j[0]["lon"]
+
+
+def nominatim_reverse_geocode(lat, lon):
+    r = core.networking.get_url(
+        "https://nominatim.openstreetmap.org/reverse?lat={}&lon={}&format=jsonv2".format(
+            lat, lon
+        )
+    )
+    return json.loads(r.text)
 
 
 def get_location(no_sys_loc):
@@ -25,48 +48,40 @@ def get_location(no_sys_loc):
     return core.get_location(no_sys_loc)
 
 
-def get_coordinates(l: str):
-    attempt_cache = core.caching.read("location" + l)
+def get_coordinates(location_string: str):
+    attempt_cache = core.caching.read("location" + location_string)
     if attempt_cache is None:
         if settings.BING_MAPS_API_KEY != "":
-            geolocator = Bing(
-                api_key=settings.BING_MAPS_API_KEY, user_agent="weathercli"
-            )
+            try:
+                coordinates = bing_maps_location_query(location_string)
+            except:
+                coordinates = nominatim_geocode(location_string)
         else:
-            geolocator = Nominatim(user_agent="weathercli")
-        try:
-            coordinates = geolocator.geocode(l, timeout=10000)
-        except:
-            geolocator = Nominatim(user_agent="weathercli")
-            coordinates = geolocator.geocode(l, timeout=10000)
+            coordinates = nominatim_geocode(location_string)
         if coordinates is None:
             raise LookupError("No such place exists")
-        r_value = [str(coordinates.latitude), str(coordinates.longitude)]
-        core.caching.write("location" + l.lower().strip(), ",".join(r_value))
-        return r_value
+        core.caching.write(
+            "location" + location_string.lower().strip(), ",".join(coordinates)
+        )
+        return coordinates
     else:
-        t = threading.Thread(target=core.caching.update_hits, args=["location" + l])
+        t = threading.Thread(
+            target=core.caching.update_hits, args=["location" + location_string]
+        )
         t.start()
         return attempt_cache.split(",")
 
 
 def reverse_location(latitude: float, longitude: float) -> tuple[str, str]:
-    ctx = ssl.create_default_context(cafile=certifi.where())
-    geopy.geocoders.options.default_ssl_context = ctx
     k = str(latitude) + "," + str(longitude)
     attempt_cache = core.caching.read("coordinates" + k)
     if attempt_cache is None:
-        geolocator = Nominatim(user_agent="weathercli")
-        place: Location = geolocator.reverse(k, timeout=10000)
-        if place is None:
-            raise LookupError("No such place exists")
-        del place.raw["licence"]
-        reversed_location = place.raw
-        country = reversed_location["address"]["country"]
-        if "city" in reversed_location["address"]:
-            region = reversed_location["address"]["city"]
-        elif "county" in reversed_location["address"]:
-            region = reversed_location["address"]["county"]
+        place = nominatim_reverse_geocode(latitude, longitude)
+        country = place["address"]["country"]
+        if "city" in place["address"]:
+            region = place["address"]["city"]
+        elif "county" in place["address"]:
+            region = place["address"]["county"]
         else:
             region = ""
         core.caching.write("coordinate" + k, region + ",?`|" + country)
--- noaa.py
+++ noaa.py
@@ -1,21 +1,10 @@
 """Historical Weather Data"""
-import ssl
-
-import certifi
 import core
-import geopy
 from core import networking
-from geopy import Nominatim
 
 
 class NOAA:
     def __init__(self, loc):
-        ctx = ssl.create_default_context(cafile=certifi.where())
-        geopy.geocoders.options.default_ssl_context = ctx
-        geolocator = Nominatim(user_agent="weathercli/0", scheme="http")
-        location = geolocator.reverse(loc[0] + ", " + loc[1])
-        country = location.raw["address"]["country"]
-        region = location.raw["address"]["city"]
         get_point = networking.get_url(
             "https://www.ncei.noaa.gov/cdo-web/api/v2/datasets"
         ).text
