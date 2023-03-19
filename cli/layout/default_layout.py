316caf0530e6fb94ecb05b41b9e82ce571c31c2c
Ashwin Naren <arihant2math@gmail.com>
2023-03-18 17:50:57 -0700

    added shortcuts to layout
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- cli/layout/default_layout.py
+++ cli/layout/default_layout.py
@@ -1,7 +1,7 @@
 layout = {
     "version": 3,
     "layout": [
-        ["Weather for ", "@region", ", ", "@country"],
+        "Weather for {@region}, {@country}",
         [
             {
                 "type": "variable",
@@ -16,27 +16,11 @@ layout = {
                 "color": "LIGHTMAGENTA_EX",
             }
         ],
-        [
-            "Temperature: ",
-            "@current_weather.temperature|° F|° C",
-            " with a low of ",
-            "@current_weather.min_temp|° F|° C",
-            " and a high of ",
-            "@current_weather.max_temp|° F|° C",
-            ", feels like ",
-            "@current_weather.feels_like|° F|° C",
-        ],
-        [
-            "Wind: ",
-            "@current_weather.wind.speed| mph| km/h",
-            " at ",
-            "@current_weather.wind.heading|°",
-        ],
-        ["Cloud Cover: ", "@current_weather.cloud_cover|%"],
-        [
-            "Dew Point: ",
-            "@current_weather.dewpoint|° F|° C",
-        ],
-        ["AQI: ", "#color_aqi|@current_weather.aqi"],
+        "Temperature: {@current_weather.temperature|° F|° C} with a low of {@current_weather.min_temp|° F|° C}, "
+        "and a high of {@current_weather.max_temp|° F|° C}, feels like {@current_weather.feels_like|° F|° C}",
+        "Wind: {@current_weather.wind.speed| mph| km/h} at {@current_weather.wind.heading|°}",
+        "Cloud Cover: {@current_weather.cloud_cover|%}",
+        "Dew Point: {@current_weather.dewpoint|° F|° C}",
+        "AQI: {#color_aqi|@current_weather.aqi}",
     ],
 }
