e655837197f3b44a8c592e07c8cfd1dbb4afe8c1
Ashwin Naren <arihant2math@gmail.com>
2023-03-19 20:57:01 -0700

    removed settings test
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,5 @@
 from click.testing import CliRunner
 
-from cli.local import settings
 from weather import main
 
 
@@ -9,11 +8,3 @@ def test_main():
     result = runner.invoke(main)
     assert not result.exception
     assert result.exit_code == 0
-
-
-def test_settings():
-    settings.store_key("FOO", "bar")
-    assert settings.get_key("FOO", "not bar") == "bar"
-    assert settings.get_key("FOOBAR", "BARFOO") == "BARFOO"
-    settings.delete_key("FOO")
-    settings.delete_key("FOOBAR")
