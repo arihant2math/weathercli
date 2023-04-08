cc622f03b6df18fb667948eba5f6d13e97029be1
Ashwin Naren <arihant2math@gmail.com>
2023-04-07 18:14:27 -0700

    another linux build fix
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- src/local.rs
+++ src/local.rs
@@ -1,6 +1,6 @@
 pub mod cache;
 pub mod settings;
 pub mod weather_file;
-#[cfg(target_os = "unix")]
+#[cfg(all(unix, not(any(target_os = "macos", target_os = "ios"))))]
 mod xdg_user_dirs;
 pub(crate) mod dirs;
