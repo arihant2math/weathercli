02677e47771525441c3bc57edd80cf8d44c52fc4
Ashwin Naren <arihant2math@gmail.com>
2023-04-24 14:57:56 -0700

    log4rs setup
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- bin/weather.rs
+++ bin/weather.rs
@@ -1,9 +1,16 @@
 use std::{fs, io};
 
 use clap::Parser;
+use log4rs::append::console::{ConsoleAppender, Target};
+use log4rs::append::file::FileAppender;
+use log4rs::Config;
+use log4rs::config::{Appender, Root};
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::filter::threshold::ThresholdFilter;
+use log::LevelFilter;
 
 use weather_core::cli::{App, Command, datasource_from_str};
-use weather_core::cli::commands::{config, credits, setup, update, weather};
+use weather_core::cli::commands::{credits, setup, update, weather};
 use weather_core::dynamic_loader::ExternalBackends;
 use weather_core::local::cache::prune_cache;
 use weather_core::local::dirs::home_dir;
@@ -37,6 +44,42 @@ fn is_ext(f: &io::Result<fs::DirEntry>) -> bool {
 }
 
 fn main() {
+    let level = LevelFilter::Info;
+    let file_path = "/tmp/foo.log";
+
+    // Build a stderr logger.
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    // Logging to log file.
+    let logfile = FileAppender::builder()
+        // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build(file_path)
+        .unwrap();
+
+    // Log Trace level output to file where trace is the default level
+    // and the programmatically specified level to stderr.
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(level)))
+                .build("stderr", Box::new(stderr)),
+        )
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .appender("stderr")
+                .build(LevelFilter::Trace),
+        )
+        .unwrap();
+
+    // Use this to change log levels at runtime.
+    // This means you can change the default log level to trace
+    // if you are trying to debug an issue and need more logs on then turn it off
+    // once you are done.
+    let _handle = log4rs::init_config(config)?;
+
     let args = App::parse();
     let settings = Settings::new();
     let mut true_metric = settings.internal.metric_default;
@@ -74,7 +117,7 @@ fn main() {
                     args.global_opts.json,
                     custom_backends
                 ),
-                Command::Config(opts) => config(opts.key, opts.value),
+                Command::Config(opts) => weather_core::cli::commands::config(opts.key, opts.value),
                 Command::Settings => weather_core::open_settings_app(),
                 Command::ClearCache => {
                     let mut f = WeatherFile::new("d.cache");
