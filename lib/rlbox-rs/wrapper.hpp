5bda0b3f385e78e106c0dcee9cdc54f01d249f8a
Ashwin Naren <arihant2math@gmail.com>
2023-05-02 21:01:34 -0700

    bump
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

---
+++ lib/rlbox-rs/wrapper.hpp
@@ -0,0 +1,5 @@
+#define RLBOX_SINGLE_THREADED_INVOCATIONS
+
+// Include these header files (see rlbox docs)
+#include "rlbox/code/include/rlbox.hpp"
+#include "rlbox/code/include/rlbox_noop_sandbox.hpp"
