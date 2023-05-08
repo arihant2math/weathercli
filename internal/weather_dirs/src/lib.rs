b671bc440af22adbf22afff9cc9cfe0d3010b84b
Ashwin Naren <arihant2math@gmail.com>
2023-05-08 15:25:17 -0700

    bumps
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

---
+++ internal/weather_dirs/src/lib.rs
@@ -0,0 +1,20 @@
+use std::path::PathBuf;
+pub type Result<T> = std::result::Result<T, weather_error::Error>;
+
+pub fn weathercli_dir() -> crate::Result<PathBuf> {
+    Ok(home::home_dir()
+        .ok_or("Home dir not found")?
+        .join(".weathercli"))
+}
+
+pub fn layouts_dir() -> crate::Result<PathBuf> {
+    Ok(weathercli_dir()?.join("layouts"))
+}
+
+pub fn custom_backends_dir() -> crate::Result<PathBuf> {
+    Ok(weathercli_dir()?.join("custom_backends"))
+}
+
+pub fn resources_dir() -> crate::Result<PathBuf> {
+    Ok(weathercli_dir()?.join("resources"))
+}
