dcb501ce9b79287ffcdd7e468f32ee7615531cb0
Ashwin Naren <arihant2math@gmail.com>
2023-11-06 10:18:22 -0800

    terminal exports
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- internal/terminal/src/lib.rs
+++ internal/terminal/src/lib.rs
@@ -2,9 +2,11 @@ pub mod color;
 pub mod prompt;
 use crossterm;
 
-
-/// Gets the terminal size (width, height)
-pub fn terminal_size() -> crate::Result<(u16, u16)> {
-    let (w, h) = crossterm::terminal::size()?;
-    Ok((w, h))
-}
+pub use crossterm::terminal::EnterAlternateScreen as enter_alternate_screen;
+pub use crossterm::terminal::LeaveAlternateScreen as leave_alternate_screen;
+pub use crossterm::terminal::Clear as clear;
+pub use crossterm::terminal::ClearType as clear_type;
+pub use crossterm::terminal::size as terminal_size;
+pub use crossterm::terminal::window_size as terminal_window_size;
+pub use crossterm::execute;
+pub use crossterm::cursor::MoveTo as move_to;
