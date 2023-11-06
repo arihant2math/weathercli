5e1ed5fbbe0c101b3c05ef1a95ee91e31566e0a1
Ashwin Naren <arihant2math@gmail.com>
2023-11-05 22:19:46 -0800

    disabled layout network access
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- internal/cli/src/layout/util.rs
+++ internal/cli/src/layout/util.rs
@@ -12,13 +12,15 @@ pub fn color_aqi(aqi: u8) -> crate::Result<String> {
 }
 
 fn url_validator(u: &str) -> bool {
-    let r = Regex::new(r"https?://(www\d?\.)?\w+\.\w+").expect("Regex failed (bug)");
+    let re: &str = r"https?://(www\d?\.)?\w+\.\w+";
+    let r = Regex::new(re).expect("Regex failed to compile (bug)");
     r.is_match(u)
 }
 
 pub fn image(source: String, scale: f64) -> crate::Result<String> {
     let is_url = url_validator(&source);
     if is_url {
+                    return Err("layout networking has been temporarily disabled".to_string())? ; // TODO: re-enable networking
         let response = networking::get_url(&source, None, None, None)?;
         let mut f = fs::OpenOptions::new()
             .write(true)
