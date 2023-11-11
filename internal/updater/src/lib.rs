30c288861802f1c54de8b7b708e2d1006a7df339
Ashwin Naren <arihant2math@gmail.com>
2023-11-10 22:07:25 -0800

    deleted unused functions
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- internal/updater/src/lib.rs
+++ internal/updater/src/lib.rs
@@ -26,36 +26,6 @@ pub fn get_latest_version() -> crate::Result<String> {
     }
 }
 
-pub fn get_latest_updater_version() -> crate::Result<String> {
-    let data = networking::get_url(
-        "https://arihant2math.github.io/weathercli/index.json",
-        None,
-        None,
-        None,
-    );
-    let json: HashMap<String, String> = unsafe { simd_json::from_str(&mut data?.text) }?;
-    Ok(json
-        .get("updater-version")
-        .ok_or("getting updater-version key failed")?
-        .to_string())
-}
-
-/// Downloads the OS specific updater
-pub fn get_updater(path: String) -> crate::Result<()> {
-    let url = format!(
-        "https://arihant2math.github.io/weathercli/{}",
-        CONFIG.updater_file_name
-    );
-    let data = networking::get_url(url, None, None, None)?.bytes;
-    let mut file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(path)?;
-    file.write_all(&data)?;
-    Ok(())
-}
-
 pub struct Config<'a> {
     pub weather_file_name: &'a str,
     pub weather_d_file_name: &'a str,
