3805a2e4ee38386a8fda941720564decbaf15c50
Ashwin Naren <arihant2math@gmail.com>
2023-12-10 19:04:26 -0800

    more terminal formatting options
    Signed-off-by: Ashwin Naren <arihant2math@gmail.com>

--- internal/terminal/src/color.rs
+++ internal/terminal/src/color.rs
@@ -1,3 +1,10 @@
+pub const BOLD: &str = "\x1b[1m";
+pub const ITALIC: &str = "\x1b[2m";
+pub const UNDERLINE: &str = "\x1b[4m";
+pub const BLINK: &str = "\x1b[5m";
+pub const STRIKETHROUGH: &str = "\x1b[9m";
+
+
 pub const RESET: &str = "\x1b[0m";
 pub const FORE_RESET: &str = "\x1b[39m";
 pub const BACK_RESET: &str = "\x1b[49m";
